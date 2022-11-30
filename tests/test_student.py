d8d102fd9f35c75676de54969d88f5348d117f9f
larissa <larza.ault@gmail.com>
2022-11-30 10:58:13 -0800

    blah2

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,4 +1,4 @@
-from .school_schedule.student import Student
+from school_schedule.student import Student
 
 # unit tests
 def test_student_initialization():
@@ -23,5 +23,47 @@ def test_student_initialization():
     assert ada_std.name == 'Samara'
     assert ada_std.grade == 'junior'
     assert 'Pre-Calc' in ada_std.classes
+
+def test_instance_method_add_classes():
+    # arrange
+    samara = {'name': "Samara",
+              'grade': "junior",
+              'classes': [
+                  "Pre-Calc",
+                  "English III",
+                  "World History",
+                  "Gym",
+                  "Chemistry",
+                  "Music Composition"
+              ]
+              }
+
+    # Act
+    ada_std = Student(samara['name'], samara['grade'], samara['classes'])
+    ada_std.add_classes('Software Engineering')
+
+    # Assert
+    assert 'Software Engineering' in ada_std.classes
+       
+
+def test_instance_method_get_num_classes():
+    # arrange
+    samara = {'name': "Samara",
+              'grade': "junior",
+              'classes': [
+                  "Pre-Calc",
+                  "English III",
+                  "World History",
+                  "Gym",
+                  "Chemistry",
+                  "Music Composition"
+              ]
+              }
+
+    # Act
+    ada_std = Student(samara['name'], samara['grade'], samara['classes'])
+    ada_std.get_num_classes()
+
+def test_instance_method_summary():
     
     
